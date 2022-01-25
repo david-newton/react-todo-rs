c797076b1fee9ce5c7866bd47ac459773edddc2d
David Newton <dtnewton83@icloud.com>
2022-01-24 20:45:12 -0500

    working with redux thunk

--- src/todolist/ToDoList.js
+++ src/todolist/ToDoList.js
@@ -9,10 +9,12 @@ import ListItem from './ListItem';
 //actions
 import { removeTask, completeTask, completeAll } from './actions/actions';
 
+import { displayAlert } from './thunks/thunks';
+
 //styles
 import './ToDoList.css';
 
-const ToDoList = ({ listItems = [], onDeleteClicked, onCompleteClicked, onCompleteAllClicked }) => {
+const ToDoList = ({ listItems = [], onDeleteClicked, onCompleteClicked, onCompleteAllClicked, onDisplayAlertClicked }) => {
     //TODO - persistor can cause listItems to be undefined/null - should protect against this or remove in memory persisting
     const completeAllButton = 
         listItems.some(item => !item.isComplete) ?
@@ -26,7 +28,7 @@ const ToDoList = ({ listItems = [], onDeleteClicked, onCompleteClicked, onComple
         <div className="todo-list-wrapper">
             <h1 className="form-title">Tasks</h1>
             <NewListForm />
-            {listItems.map(item => <ListItem item={item} onDeleteClicked={onDeleteClicked} onCompleteClicked={onCompleteClicked}/>)}
+            {listItems.map(item => <ListItem key={item.data} item={item} onDeleteClicked={onDeleteClicked} onCompleteClicked={onDisplayAlertClicked}/>)}
             {completeAllButton}
         </div>
     );
@@ -40,6 +42,7 @@ const mapDispatchToProps = dispatch => ({
     onDeleteClicked: data => dispatch(removeTask(data)),
     onCompleteClicked: data => dispatch(completeTask(data)),
     onCompleteAllClicked: () => dispatch(completeAll()),
+    onDisplayAlertClicked: () => dispatch(displayAlert()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ToDoList);
